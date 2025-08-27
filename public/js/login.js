f9cf654e50981899561ca3106d831c8089c14b49
fiovargas <fiovargasp31@gmail.com>
2025-08-27 09:15:15 -0600

    actualizacion 1

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmarPassword = document.getElementById("confirmarPassword");
+const Registro = document.getElementById("Registro");
+const estudiante = document.getElementById("estudiante");
+const administrador = document.getElementById("administrador");
+const registrarse = document.getElementById("registrarse");
+
+
