e95a519c62ffe8dde3cc5f5636d939ac5549b81c
fiovargas <fiovargasp31@gmail.com>
2025-08-28 10:47:53 -0600

    actualizacion

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,3 +1,4 @@
+// Importamos las funciones desde el archivo de servicios.
 import{getUsers, postUsers, deleteUsers, putUsers} from "../services/ServicesUser.js"
 
 const usuario = document.getElementById("name");
@@ -10,16 +11,18 @@ const seleccionador = document.getElementById("seleccionador");
 
 registrarse.addEventListener("click", async function () {
 
+    // Verificamos si las contraseñas coinciden.
 if (password.value !== confirmarPassword.value) {
     Toastify({
         text: "Las contraseñas no coinciden",
         duration: 3000
     }).showToast();
 
-    return;
+    return; // Si no coinciden, detenemos el registro.
 };
     console.log(seleccionador.value);
     
+    // Creamos un objeto con los datos ingresados.
     const Regis = {
         usuario:usuario.value,
         email:email.value,
@@ -27,24 +30,27 @@ if (password.value !== confirmarPassword.value) {
         tipoUser:seleccionador.value
     };
 
-    console.log(Regis);
+    console.log(Regis); // Podemos ver en consola los datos a enviar
     
+     // Enviamos los datos al backend usando la función postUsers().
     const respuesta = await postUsers(Regis);
 
-    console.log(respuesta);
+    console.log(respuesta); // Mostramos la respuesta del servidor.
 
+    // Mostramos un mensaje de éxito.
      Toastify({
         text: "Registro guardado con exito",
         duration: 3000
     }).showToast();
 
-    limpiarCampos();
+    limpiarCampos(); // Limpiamos los campos del formulario
 
     setTimeout(() => {
         window.location.href = "../pages/login.html";
-    }, 3000);
+    }, 1000);
 });
 
+// Función para limpiar los campos del formulario después del registro.
 function limpiarCampos() {
     usuario.value = "";
     email.value = "";
