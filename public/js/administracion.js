b5dc45ea4cdbc393656511dd6a66b30b3b5a583d
norafigueroa <nelizabeth.figueroa@gmail.com>
2025-08-30 21:53:03 -0600

    Nora

--- public/js/administracion.js
+++ public/js/administracion.js
@@ -1,38 +1,65 @@
+//Constantes de menú
 const inicio = document.getElementById("inicio");
 const historial = document.getElementById("historial");
-const aprobacion = document.getElementById("aprobacion");
-const registroAdmi = document.getElementById("registroAdmi");
-const historialContenedor = document.getElementById("historialContenedor")
+const pendientes = document.getElementById("pendientes");
+const registro = document.getElementById("registro");
+const cerrar = document.getElementById("cerrar")
 const contenido = document.getElementById("contenido")
+//Constantes de contenedores
 const inicioContenedor = document.getElementById("inicioContenedor")
-const cerrar = document.getElementById("cerrar");
+const historialContenedor = document.getElementById("historialContenedor")
+const pendientesContenedor = document.getElementById("pendientesContenedor")
+const registroContenedor = document.getElementById("registroContenedor")
+//Botón de registro de nuevo administrador
+const registrarse = document.getElementById("registrarse");
 
-// Funciones de redirección
-inicio.addEventListener("click", () => {
-    window.location.href = "../pages/administracion.html";
+function ocultarTodo() {
+  inicioContenedor.classList.add("oculto");
+  historialContenedor.classList.add("oculto");
+  pendientesContenedor.classList.add("oculto");
+  registroContenedor.classList.add("oculto");
+}
+
+///////Comienza Menú Inicio///////
+inicio.addEventListener("click", () => { 
+  ocultarTodo();
+  inicioContenedor.classList.remove("oculto");
+
 });
+///////Termina Menú Inicio///////
 
+
+///////Comienza Menú historial///////
 historial.addEventListener("click", () => {
-    /* window.location.href = "../pages/historial.html"; */
-    inicioContenedor.textContent = ""
-    historialContenedor.style.display = "block"
-
-    
+    ocultarTodo();
+    historialContenedor.classList.remove("oculto");
 
 });
+///////Termina Menú Historial///////
 
-aprobacion.addEventListener("click", () => {
-    window.location.href = "../pages/aprobacion.html";
+
+///////Comienza Menú pendientes///////
+pendientes.addEventListener("click", () => { 
+    ocultarTodo();
+    pendientesContenedor.classList.remove("oculto");
 });
+///////Termina Menú Historial///////
 
-registroAdmi.addEventListener("click", () => {
-    window.location.href = "../pages/registroAdmi.html";
+
+///////Comienza Menú registro///////
+registro.addEventListener("click", () => { 
+    ocultarTodo();
+    registroContenedor.classList.remove("oculto");
 });
+///////Termina Menú registro///////
 
-cerrar.addEventListener("click", () => {
+
+///////Comienza Menú cerrar///////
+cerrar.addEventListener("click", () => { 
     Toastify({
             text: "Cerraste la sesión",
             duration: 3000
     }).showToast();
     window.location.href = "../pages/login.html";
 });
+///////Termina Menú cerrar///////
